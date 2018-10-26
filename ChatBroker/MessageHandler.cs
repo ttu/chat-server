57629555682e1f664abd2cd4045b978003a41ef9
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2018-10-26 17:07:50 +0300

    Add ChatBroker project

---
+++ ChatBroker/MessageHandler.cs
@@ -0,0 +1,43 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ChatBroker
+{
+    public class MessageHandler
+    {
+        private readonly IConnection _connection;
+        private readonly string _queueName = "hello";
+
+        public MessageHandler(IConnection connection)
+        {
+            _connection = connection;
+            Start();
+        }
+
+        public void Start()
+        {
+            // Loop messages from the queue
+
+            // Start new Thread for each meassage
+            var channel = _connection.CreateModel();
+            channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                HandleMessage(message);
+            };
+
+            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+        }
+
+        public void HandleMessage(string message)
+        {
+            // Get server url
+        }
+    }
+}
---
+++ ChatBroker/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMQ.Client;
+using System;
+
+namespace ChatBroker
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(new ConnectionFactory() { HostName = Configuration.GetValue<string>("Connections:RabbitMQ") });
+            services.AddSingleton(s => s.GetService<ConnectionFactory>().CreateConnection());
+
+            services.AddSingleton(s => new MessageHandler(s.GetService<IConnection>()));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello from ChatBroker");
+            });
+
+            var handler = services.GetService<MessageHandler>();
+        }
+    }
+}
